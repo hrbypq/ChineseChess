da52cc6c34904c398c095e7f00bba985a8acedc9
ypq <hrbypq@163.com>
2018-12-04 22:54:45 +0800

    format modified

--- src/pieces/Pawn.java
+++ src/pieces/Pawn.java
@@ -5,43 +5,37 @@ package pieces;
  * @author ypq
  * @version1.0
  */
-public class Pawn extends ChessPiece
-{
-	public Pawn(int init_row,int init_col,boolean init_isRed,String aname)
-	{
+public class Pawn extends ChessPiece{
+	public Pawn(int init_row,int init_col,boolean init_isRed,String aname){
 		super(init_row,init_col,init_isRed,aname);
 	}
 
 	@Override
-	public boolean isLegalMove(int toRow, int toCol)
-	{
-		if(this.isRed)   //红棋情形
-		{
-
+	public boolean isLegalMove(int toRow, int toCol){
+		 //红棋情形
+		if(this.isRed){
 			if(toRow-this.row<0||toRow-this.row<-1)
 				return false;
-			if(this.row>=5)    //红兵 未过河
-			{
+			//红兵 未过河
+			if(this.row>=5){
 				if(this.col!=toCol)
 					return false;
 			}
-			else
-			{
+			else{
 				if(toCol-this.col>1||toCol-this.col<-1)
 					return false;
 			}
 		}
-		else  //黑棋情形
-		{
+		//黑棋情形
+		else{
 			if(toRow-this.row<0||toRow-this.row>1)
 				return false;
-			if(this.row<=4)    //黑兵 未过河
-			{
+			//黑兵 未过河
+			if(this.row<=4){
 				if(this.col!=toCol)
 					return false;
 			}
-			else
-			{
+			else{
 				if(toCol-this.col>1||toCol-this.col<-1)
 					return false;
 			}
